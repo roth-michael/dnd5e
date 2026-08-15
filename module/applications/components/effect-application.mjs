c90fc5e8e81d8e3e1d18f9993be3d14702fc2efc
Michael <michaelhroth624@gmail.com>
2026-08-15 09:18:27 -0400

    rewrite PR on top of new chat cards

--- module/applications/components/effect-application.mjs
+++ module/applications/components/effect-application.mjs
@@ -343,10 +343,6 @@ export default class EffectApplicationElement extends TargetedApplicationMixin(C
       }, effectFlags) };
     }
 
-    if ( !game.user.isGM && concentration && !concentration.isOwner ) {
-      throw new Error(_loc("DND5E.EFFECT.Application.Warning.Concentration"));
-    }
-
     // Otherwise, create a new effect on the target
     return { action: "create", data: foundry.utils.mergeObject({
       ...effect.toObject(),
